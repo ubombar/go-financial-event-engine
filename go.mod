32d4c4e46e572f2761ac7d482c73272b905560c8
ubombar <ufukbombar@gmail.com>
2024-01-20 15:20:20 +0100

    Add example to main

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ubombar/go-financial-event-engine
 go 1.21.6
 
 require (
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
--- gofee.go
+++ gofee.go
@@ -3,8 +3,41 @@ Copyright © 2024 Ufuk Bombar ufukbombar@gmail.com
 */
 package main
 
-import "github.com/ubombar/go-financial-event-engine/cmd"
+import (
+	"fmt"
+	"time"
+
+	"github.com/ubombar/go-financial-event-engine/pkg/gofee"
+)
+
+func NewGenesisEventCallback(initialValue int) func(s gofee.State) gofee.State {
+	return func(s gofee.State) gofee.State {
+		state := gofee.NewConcreeteState()
+		state.Accounts("default").SetValue(initialValue)
+		return state
+	}
+}
+
+func NewConstantEventCallback(valueDifference int) func(s gofee.State) gofee.State {
+	return func(s gofee.State) gofee.State {
+		state := gofee.NewConcreeteState()
+		state.Accounts("default").SetValue(state.Accounts("default").Value() + valueDifference)
+		return state
+	}
+}
 
 func main() {
-	cmd.Execute()
+	// cmd.Execute()
+
+	rootEvent := gofee.NewConstantEvent(time.Now(), NewGenesisEventCallback(100_00))
+
+	event1 := gofee.NewConstantEvent(time.Now(), NewConstantEventCallback(+100_00))
+	event2 := gofee.NewConstantEvent(time.Now(), NewConstantEventCallback(-22_00))
+	event3 := gofee.NewConstantEvent(time.Now(), NewConstantEventCallback(+12_00))
+
+	rootEvent.AddFork(event1)
+	rootEvent.AddFork(event2)
+	rootEvent.AddFork(event3)
+
+	fmt.Printf("rootEvent: %v\n", rootEvent)
 }
